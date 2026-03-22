6f2425d875fab25834d9dc2502a154d0e78af338
GlucaTekmar <gianluca.colantoni@tekmarpromo.it>
2026-03-22 17:53:16 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Date, ForeignKey, TIMESTAMP
 from sqlalchemy.sql import func
-from database import Base
+from backend.database import Base
 
 
 class PDV(Base):
