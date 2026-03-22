6cd06350fb5e418709606c851f386090e0a3fdff
GlucaTekmar <gianluca.colantoni@tekmarpromo.it>
2026-03-22 18:34:12 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -10,3 +10,11 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 def root():
     return {"status": "API attiva"}
+
+@app.get("/pdv")
+def get_pdv():
+    return [
+        {"id": 1, "nome": "Milano - Coop Via Roma"},
+        {"id": 2, "nome": "Roma - Centro"},
+        {"id": 3, "nome": "Torino - Carrefour"}
+    ]
