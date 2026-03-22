e9770eaaee79a81b971deaf841ca5b69a2288a9d
GlucaTekmar <gianluca.colantoni@tekmarpromo.it>
2026-03-22 17:01:57 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
+from database import engine, Base
 
+app = FastAPI()
+
+# crea le tabelle nel database
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+def root():
+    return {"status": "API attiva"}
