377a6670f46d9f08792c2224a9dcacf86efd313f
GlucaTekmar <gianluca.colantoni@tekmarpromo.it>
2026-03-24 16:05:44 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -42,7 +42,7 @@ def get_log(db: Session = Depends(get_db)):
         {
             "nome_dipendente": l.nome_dipendente,
             "pdv_id": l.pdv_id,
-            "timestamp": l.timestamp
+            "timestamp": str(l.timestamp)
         }
         for l in logs
     ]
