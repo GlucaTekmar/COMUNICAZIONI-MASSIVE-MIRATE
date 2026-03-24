56ac3ed7f279a8c4f28f18e9f591e85f7c73280d
GlucaTekmar <gianluca.colantoni@tekmarpromo.it>
2026-03-24 15:07:34 +0100

    Update app_dipendenti.py

--- frontend/app_dipendenti.py
+++ frontend/app_dipendenti.py
@@ -7,6 +7,8 @@ st.set_page_config(page_title="Selezione PDV")
 
 st.title("SELEZIONA PUNTO VENDITA")
 
+nome = st.text_input("Inserisci nome e cognome")
+
 response = requests.get(f"{API_URL}/pdv")
 pdv_list = response.json()
 
@@ -14,5 +16,15 @@ nomi_pdv = [p["nome"] for p in pdv_list]
 
 pdv = st.selectbox("Scegli PDV", nomi_pdv)
 
-if pdv:
+if pdv and nome:
+    pdv_id = next(p["id"] for p in pdv_list if p["nome"] == pdv)
+
+    requests.post(
+        f"{API_URL}/log",
+        params={
+            "pdv_id": pdv_id,
+            "nome_dipendente": nome
+        }
+    )
+
     st.success(f"Hai selezionato: {pdv}")
