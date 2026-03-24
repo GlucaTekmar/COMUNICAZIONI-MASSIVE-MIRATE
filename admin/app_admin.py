0031c88e20acb260eec464875ca25d49a562dcb6
GlucaTekmar <gianluca.colantoni@tekmarpromo.it>
2026-03-24 15:23:41 +0100

    Update app_admin.py

--- admin/app_admin.py
+++ admin/app_admin.py
@@ -1,1 +1,12 @@
+@app.get("/log")
+def get_log(db: Session = Depends(get_db)):
+    logs = db.query(Log).all()
 
+    return [
+        {
+            "nome_dipendente": l.nome_dipendente,
+            "pdv_id": l.pdv_id,
+            "timestamp": l.timestamp
+        }
+        for l in logs
+    ]
