6b68af360d0ef777166f943c99552c8ea7d35f89
GlucaTekmar <gianluca.colantoni@tekmarpromo.it>
2026-03-25 15:42:08 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from backend.routes import router as api_router
+from backend.database import engine, Base
 
 app = FastAPI()
+
+# CREA TABELLE AUTOMATICAMENTE
+Base.metadata.create_all(bind=engine)
 
 app.include_router(api_router)
 
